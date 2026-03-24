8e3587085ba28b1471b7fba59d80f8e026e58e8f
Alexey Yurchenko <homonoidian@proton.me>
2026-03-24 03:46:06 +0300

    Add concatenation and slice methods to `Pf::Kit::HybridArray`
    I don't know how bug-free they are. Let's just hope there are no bugs
    huh!

--- src/permafrost/kit/hybrid_array.cr
+++ src/permafrost/kit/hybrid_array.cr
@@ -1,5 +1,5 @@
 module Pf::Kit
-  # A dynamic array that stores *N* x *T*s in a fixed-size buffer (most
+  # A dynamic array which stores *N* x *T*s in a fixed-size buffer (inline; most
   # often allocated on the stack), and spills over to heap.
   #
   # Inspiration: One of Walter Bright talks, at https://www.youtube.com/watch?v=_PB6Hdi4R7M&t=2606s
@@ -22,7 +22,7 @@ module Pf::Kit
     include Indexable::Mutable(T)
 
     # :nodoc:
-    INITIAL_SPILL_CAPACITY = 8
+    INITIAL_SPILL_CAPACITY = 8u32
 
     # @type_id : Int32
     @bufsize : UInt32
@@ -78,14 +78,19 @@ module Pf::Kit
 
     # Inserts *value* at the back of this array.
     def push(value : T) : self
-      if @bufsize < N
+      if @bufsize + 1 <= N
         @buffer[@bufsize] = value
         @bufsize += 1
         return self
       end
 
-      unless @spillsize < @spillcap
-        @spillcap = Math.max(INITIAL_SPILL_CAPACITY, @spillcap * 1.5).to_u32
+      unless @spillsize + 1 <= @spillcap
+        if @spillcap.zero?
+          @spillcap = INITIAL_SPILL_CAPACITY
+        else
+          @spillcap += @spillcap//2 # 1.5x
+        end
+
         @spill = @spill.realloc(@spillcap)
       end
 
@@ -98,6 +103,49 @@ module Pf::Kit
     # :ditto:
     def <<(value : T) : self
       push(value)
+    end
+
+    # Pushes all elements in *other* to this array.
+    #
+    # This overload is optimized to allocate the necessary memory upfront,
+    # and otherwise use `memcpy`, which I expect to be faster than copying
+    # in a loop (although I'm not particularly sure).
+    #
+    # WARNING: *other* must not overlap with this array's memory (uses `Pointer#copy_to`).
+    def concat(other : Slice(T)) : self
+      if N - @bufsize > 0
+        n = Math.min(N - @bufsize, other.size)
+        other.copy_to(@buffer + @bufsize, n)
+        other += n
+        @bufsize += n
+      end
+
+      if other.empty?
+        return self
+      end
+
+      unless @spillsize + other.size <= @spillcap
+        @spillcap = @spillsize + other.size
+        @spillcap += @spillcap//2 # 1.5x
+        @spill = @spill.realloc(@spillcap)
+      end
+
+      other.copy_to(@spill + @spillsize, other.size)
+      @spillsize += other.size
+
+      self
+    end
+
+    # :ditto:
+    def concat(other : Array(T)) : self
+      concat(Slice.new(other.to_unsafe, other.size, read_only: true))
+    end
+
+    # Pushes all elements in *other* to this array.
+    def concat(other : Indexable(T)) : self
+      other.each { |object| push(object) }
+
+      self
     end
 
     # Removes and returns the last value of this array. Returns `nil` if
@@ -142,6 +190,75 @@ module Pf::Kit
       if @spillsize > 0
         @spill.copy_to(target + @bufsize, @spillsize)
       end
+    end
+
+    # Returns a copy of this array's content as a slice.
+    def to_slice : Slice(T)
+      target = Pointer(T).malloc(size)
+      unsafe_copy_to(target)
+
+      Slice.new(target, size)
+    end
+
+    # Returns a copy of this array's content as a read-only slice.
+    def to_readonly_slice : Slice(T)
+      slice = to_slice
+
+      Slice.new(slice.to_unsafe, slice.size, read_only: true)
+    end
+
+    # Returns a slice referencing the content of this array.
+    #
+    # This overload is most beneficial when there is spillover. When there is none,
+    # this overload is the same as calling `to_slice`.
+    #
+    # Since spillover reserves 1.5x more memory than strictly necessary, we can
+    # use that extra memory, when possible, to memmove, and then put the array's
+    # inline (e.g. stack-allocated) content in front. This overload does exactly that.
+    #
+    # If we can't use the remaining memory (inline content doesn't fit in spillover),
+    # we *reallocate*, which means the GC may give us the same chunk of memory,
+    # but longer. Sometimes it will not.
+    #
+    # WARNING: This method alters this array's original content. You **must not**
+    # use the array after calling this method, as its spillover part is now referenced
+    # by others.
+    def to_unsafe_slice! : Slice(T)
+      if @spillsize.zero?
+        if @bufsize.zero?
+          return Slice(T).empty
+        end
+
+        target = Pointer(T).malloc(@bufsize)
+        target.copy_from(@buffer, @bufsize)
+
+        return Slice.new(target, @bufsize)
+      end
+
+      unless @bufsize + @spillsize <= @spillcap
+        @spillcap = @bufsize + @spillsize
+        @spill = @spill.realloc(@spillcap)
+      end
+
+      @spill.move_to(@spill + @bufsize, @spillsize)
+      @spill.copy_from(@buffer, @bufsize)
+      @spillsize += @bufsize
+
+      if @spillsize < @spillcap
+        @spillcap = @spillsize
+        @spill = @spill.realloc(@spillcap)
+      end
+
+      Slice.new(@spill, @spillsize)
+    end
+
+    # Returns a slice referencing the content of this array.
+    #
+    # See `to_unsafe_slice!` for info on safety.
+    def to_unsafe_readonly_slice! : Slice(T)
+      slice = to_unsafe_slice!
+
+      Slice.new(slice.to_unsafe, slice.size, read_only: true)
     end
 
     def inspect(io)
